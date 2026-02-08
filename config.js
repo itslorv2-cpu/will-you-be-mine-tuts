31e895f15fd87c9167c98d66db08042acbb13140
itslorv2-cpu <itslorv2@gmail.com>
2026-02-07 21:39:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dezmxufpa/raw/upload/v1770517337/xbMBHI0o4tY_j8eqb2", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dezmxufpa/video/upload/v1770518308/Odeal_-_Temptress_Official_Lyric_Video_mpualr.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.2                        // Volume level (0.0 to 1.0)
